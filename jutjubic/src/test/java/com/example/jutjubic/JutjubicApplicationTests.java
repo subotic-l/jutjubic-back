34d2bca0bb4a943866ce09594a16df133ec95432
subotic.l <lukasubotic34@gmail.com>
2025-12-24 22:55:48 +0100

    init

---
+++ jutjubic/src/test/java/com/example/jutjubic/JutjubicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jutjubic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JutjubicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
